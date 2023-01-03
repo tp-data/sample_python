80b737095354d6ba2ade75985389d5bd58b825a9
trevor-pahigian <trevor.pahigian@deel.com>
2023-01-02 21:09:06 -0500

    upload all initial code

---
+++ merge.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+items = pd.read_csv("items.csv")
+orders = pd.read_csv("orders.csv")
+
+orders_with_items = orders.merge(items, how='left', on='item_id')
+orders_with_items.head(5)
+
+# create datasets for just item id columns
+item_ids = items['item_id']
+orders_item_ids = orders['item_id']
+
+# create variable to store ids missing from the orders dataset
+missing_ids = item_ids[~item_ids.isin(orders_item_ids)]
+
+# grab records from items dataset where id matches missing id
+items[items['item_id'].isin(missing_ids)]
